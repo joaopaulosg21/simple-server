637010e228149f88cbdc629b9c5908b59b9daf10
joaopaulo <joaopaulosg17@gmail.com>
2024-03-14 19:14:44 -0300

    feat: Adiciona class Response

---
+++ src/main/java/projeto/server/pojos/Response.java
@@ -0,0 +1,31 @@
+package projeto.server.pojos;
+
+import java.util.Map;
+
+public class Response {
+
+    private Map<String, String> headers;
+
+    private String body;
+
+    public Response(Map<String, String> headers, String body) {
+        this.headers = headers;
+        this.body = body;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
