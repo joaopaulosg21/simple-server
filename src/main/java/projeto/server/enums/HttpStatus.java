0d6fa636187ce81dbe4f14d3a917169c2bdae942
joaopaulo <joaopaulosg17@gmail.com>
2024-03-16 10:04:15 -0300

    feat: Adicionado suporte a mais status de resposta

--- src/main/java/projeto/server/enums/HttpStatus.java
+++ src/main/java/projeto/server/enums/HttpStatus.java
@@ -1,9 +1,16 @@
 package projeto.server.enums;
 
 public enum HttpStatus {
-    OK("200 OK");
+    OK("200 OK"),
+    NOT_FOUND("404 NOT FOUND"),
+    CREATED("201 CREATED"),
+    BAD_REQUEST("400 BAD REQUEST"),
+    UNAUTHORIZED("401 UNAUTHORIZED"),
+    METHOD_NOT_ALLOWED("405 METHOD NOT ALLOWED"),
+    INTERNAL_SERVER_ERROR("500 INTERNAL SERVER ERROR");
 
     String value;
+
     HttpStatus(String value) {
         this.value = value;
     }
